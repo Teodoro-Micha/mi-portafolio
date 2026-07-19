49786749c1d8e1606a793cf3d420e6208084bec5
Teodoro-Micha <djardelmicha38@gmail.com>
2026-07-19 19:02:38 +0100

    Conectando mi portafolio index.html a mi index.js, para que la información introducida en el formulario, pueda verla en la consola

---
+++ PORTAFOLIO/index.js
@@ -0,0 +1,27 @@
+// Buscamos el formulario en el HTML usando su ID:
+const formulario = document.getElementById("formulario-contacto");
+
+// Creamos el "escuchador"; le decimos al programa que esté atento al evento '"submit":
+
+formulario.addEventListener("submit", function(Evento) {
+
+    // Para evitar que el navegador limpie el formulario antes de que el pueda leerlo:
+    Evento.preventDefault();
+
+    // Recogemos los datos que el usuario ha escrito:
+    const Datos = new FormData(formulario);
+
+    const Nombre = Datos.get("nombre");
+    const Email = Datos.get("correo");
+    const Mensaje = Datos.get("mensaje");
+
+    console.log("Formulario rellenado con éxito");
+    console.log("Nombre introducido:", Nombre)
+    console.log("Correo introducido:", Email);
+    console.log("Mensaje introducido:", Mensaje);
+
+    alert("Gracias, he recibido tu mensaje");
+
+
+
+});
