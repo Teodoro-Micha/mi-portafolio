351f9c749bcbbd79e94d4d2b6594d4c6324cbe2c
Teodoro-Micha <djardelmicha38@gmail.com>
2026-07-24 19:25:04 +0100

    Trabajo terminado, web completa a nivel de HTML, CSS y JavaScript

--- PORTAFOLIO/Aplicaciones de Hipermedia/Candidature_en_ligne_Offre_de_stage/script.js
+++ PORTAFOLIO/Aplicaciones de Hipermedia/Candidature_en_ligne_Offre_de_stage/script.js
@@ -1,9 +1,14 @@
 function verifierCandidature(event) {
+    if (event) event.preventDefault();
+
     const nom = document.getElementById("nom").value.trim();
     const prenom = document.getElementById("prenom").value.trim();
     const email = document.getElementById("email").value.trim();
     const message = document.getElementById("message").value.trim();
     const certif = document.getElementById("certif").checked;
+    const domaine = document.getElementById("domaine").value;
+    const dureeSelected = document.querySelector('input[name="duree"]:checked');
+    const cvInput = document.getElementById("cv");
     
 
 
@@ -32,6 +37,25 @@ function verifierCandidature(event) {
         return false;
     }
 
+    if (domaine === "") {
+            alert("Veuillez sélectionner un département pour le stage.");
+            return;
+    }
+
+    if (!dureeSelected) {
+            alert("Veuillez choisir une durée pour votre stage.");
+            return;
+    }
+
+    if (!cvInput || cvInput.files.length === 0) {
+    alert("Veuillez joindre votre Curriculum Vitae (CV).");
+    return;
+    }
+
+    if (message === "") {
+        alert("Veuillez rédiger une lettre de motivation.");
+        return false;
+    }
     
     if (message.length < 10) {
         alert("Votre lettre de motivation doit contenir au moins 10 caractères.");
@@ -39,7 +63,6 @@ function verifierCandidature(event) {
         return false;
     }
 
-    
     if (!certif) {
         alert("Vous devez certifier que les informations sont exactes avant d'envoyer.");
         event.preventDefault(); // Empêcher l'envoi du formulaire
@@ -52,18 +75,12 @@ function verifierCandidature(event) {
 
 // 2. Boton RÉINITIALISER
 document.addEventListener("DOMContentLoaded", () => {
-    const botonReinitialiser = document.getElementById('boton_reinitialiser');
+    const botonReinitialiser = document.getElementById("boton_reinitialiser");
 
-    if (botonReinitialiser) {
-        botonReinitialiser.addEventListener("click", (event) => {
-            event.preventDefault();
-
-        // confirmer avant de supprimer:
+    botonReinitialiser.addEventListener("click", () => {
         if (confirm("Êtes-vous sûr de vouloir réinitialiser le formulaire ?")) {
-            document.querySelector(".application-form");
-            if (form) {
-                form.reset();
-            }
+        
+        document.querySelector(".application-form").reset();
         }
-    })
-})
+    });      
+});
