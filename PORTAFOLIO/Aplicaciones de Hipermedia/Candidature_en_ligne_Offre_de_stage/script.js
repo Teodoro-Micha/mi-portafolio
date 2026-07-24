e0823c0f2d32772f5c24fe33ea5365b5e934a275
Teodoro-Micha <djardelmicha38@gmail.com>
2026-07-24 16:34:47 +0100

    Sigo trabajando y avanzando en esta encomienda

--- PORTAFOLIO/Aplicaciones de Hipermedia/Candidature_en_ligne_Offre_de_stage/script.js
+++ PORTAFOLIO/Aplicaciones de Hipermedia/Candidature_en_ligne_Offre_de_stage/script.js
@@ -1,12 +1,20 @@
 function verifierCandidature(event) {
-    const nom = document.getElementById('nom').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
-    const certif = document.getElementById('certif').checked;
+    const nom = document.getElementById("nom").value.trim();
+    const prenom = document.getElementById("prenom").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+    const certif = document.getElementById("certif").checked;
+    
 
 
     if (nom === "") {
-        alert("Veuillez saisir votre nom complet.");
+        alert("Veuillez saisir votre nom.");
+        event.preventDefault(); // Empêcher l'envoi du formulaire
+        return false;
+    }
+
+    if (prenom === "") {
+        alert("Veuillez saisir votre prenom.");
         event.preventDefault(); // Empêcher l'envoi du formulaire
         return false;
     }
@@ -31,13 +39,31 @@ function verifierCandidature(event) {
         return false;
     }
 
-    s
+    
     if (!certif) {
         alert("Vous devez certifier que les informations sont exactes avant d'envoyer.");
         event.preventDefault(); // Empêcher l'envoi du formulaire
         return false;
     }
 
-    
+    alert('Votre candidature a été envoyée avec succès !');
     return true;
 }
+
+// 2. Boton RÉINITIALISER
+document.addEventListener("DOMContentLoaded", () => {
+    const botonReinitialiser = document.getElementById('boton_reinitialiser');
+
+    if (botonReinitialiser) {
+        botonReinitialiser.addEventListener("click", (event) => {
+            event.preventDefault();
+
+        // confirmer avant de supprimer:
+        if (confirm("Êtes-vous sûr de vouloir réinitialiser le formulaire ?")) {
+            document.querySelector(".application-form");
+            if (form) {
+                form.reset();
+            }
+        }
+    })
+})
